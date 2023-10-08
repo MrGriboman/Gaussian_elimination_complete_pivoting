135718d697c6712c040db00ab77ebd73cb7bf5fc
MrGriboman <woewe@sdsd.com>
2023-10-08 20:29:25 +1000

    indices fix

--- src.py
+++ src.py
@@ -1,26 +1,26 @@
 import numpy as np
 
+
 def gaussian_elimination_complete_pivot(A, b):
     augmented = np.column_stack((A, b))
-    a_max = np.max(np.abs(A))
-    a_max_i = np.argmax(np.abs(A))
-    print(a_max, a_max_i)
+    for k in range(augmented.shape[0]): 
+        a_max = np.max(np.abs(augmented[:, :-1]))
+        row_index, col_index = np.unravel_index(np.argmax(np.abs(augmented[:, :-1]), axis=None), augmented[:, :-1].shape)
 
-    num_rows, num_cols = A.shape
-    row_index = a_max_i // num_cols
-    col_index = a_max_i % num_cols
+        # Swap the rows
+        print(augmented)    
+        augmented[[0, row_index]] = augmented[[row_index, 0]]
+        print(augmented)
 
-    # Swap the rows
-    print(A)
-    A[[0, row_index]] = A[[row_index, 0]]
-    print(A)
+        # Perform Gaussian elimination on rows below the first row
+        augmented[0] /= augmented[0, col_index]
+        for i in range(1, augmented.shape[0]):
+            augmented[0] / augmented[0, col_index]
+            factor = augmented[0, col_index] / augmented[i, col_index] 
+            augmented[i] *= factor
+            augmented[i] -= augmented[0, col_index]                
+        print(augmented)
 
-    # Perform Gaussian elimination on rows below the first row
-    for i in range(1, A.shape[0]):
-        factor = A[0, col_index] / A[i, col_index] 
-        A[i] *= factor
-        A[i] -= A[0, col_index]
-    print(A)
 
 n = int(input("Введите размерность \n"))
 A = []
@@ -30,7 +30,7 @@ for i in range(n):
 b = np.array(list(map(float, input("Введите вектор b\n").split())))
 A = np.array(A)
 
-x = gaussian_elimination_complete_pivot(A, b)
+x = gaussian_elimination_complete_pivot(np.copy(A), b)
 x_solve = np.linalg.solve(A, b)
 #print(f"Solution:\n{x}")
 #print(f'Solve solution:\n{x_solve}')
