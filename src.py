fd144f427aada379472aae6efb0b730cdbf8a1da
MrGriboman <woewe@sdsd.com>
2023-10-08 21:29:39 +1000

    iteration

--- src.py
+++ src.py
@@ -3,23 +3,32 @@ import numpy as np
 
 def gaussian_elimination_complete_pivot(A, b):
     augmented = np.column_stack((A, b))
-    for k in range(augmented.shape[0]): 
-        a_max = np.max(np.abs(augmented[:, :-1]))
-        row_index, col_index = np.unravel_index(np.argmax(np.abs(augmented[:, :-1]), axis=None), augmented[:, :-1].shape)
+    rows = np.copy(augmented.shape[0]) #maybe copy
+    for k in range(rows):         
+        a_max = np.max(np.abs(augmented[k:rows, :-1]))
+        row_index, col_index = np.unravel_index(
+            np.argmax(np.abs(augmented[k:rows, :-1]), axis=None),
+            augmented[k:rows, :-1].shape
+        )
+        row_index += k
+
+        print(f'row and col: {(row_index, col_index)}')
 
         # Swap the rows
-        print(augmented)    
-        augmented[[0, row_index]] = augmented[[row_index, 0]]
-        print(augmented)
+        print(f'before swap\n{augmented}')    
+        augmented[[k, row_index]] = augmented[[row_index, k]]
+        print(f'after swap\n{augmented}')
 
         # Perform Gaussian elimination on rows below the first row
-        augmented[0] /= augmented[0, col_index]
-        for i in range(1, augmented.shape[0]):
-            augmented[0] / augmented[0, col_index]
-            factor = augmented[0, col_index] / augmented[i, col_index] 
+        augmented[k] /= a_max
+        for i in range(k + 1, rows):
+            augmented[k] / a_max
+            if not augmented[i, col_index]:
+                continue
+            factor = a_max / augmented[i, col_index]
             augmented[i] *= factor
-            augmented[i] -= augmented[0, col_index]                
-        print(augmented)
+            augmented[i] -= a_max              
+        print(f'after ellimination\n{augmented}')
 
 
 n = int(input("Введите размерность \n"))
